04d76884e022b1eed4f500a90ec28105efa2b822
Ryan Gomes <ryang3550@gmail.com>
2025-11-05 19:04:23 -0300

    Cria a página de Login
    - Criação da página de login interativa e com transições fluidas
    - Importação dos icons e fonte que serão utilizados no projeto
    - Criação das folhas de estilo

---
+++ src/Login/Login.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import "./Login.css";
+
+const Login = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setIsActive(true);
+  };
+
+  const handleLoginClick = () => {
+    setIsActive(false);
+  };
+
+  return (
+    <div className={`container ${isActive ? "active" : ""}`} id="container">
+      <div className="form-container sign-up">
+        <form>
+          <h1>Criar Conta</h1>
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <i className="fa-brands fa-google-plus-g"></i>
+            </a>
+          </div>
+          <span>ou use seu e-mail para se registrar</span>
+          <input type="text" placeholder="Nome" />
+          <input type="email" placeholder="E-mail" />
+          <input type="password" placeholder="Senha" />
+          <button type="button">Cadastrar</button>
+        </form>
+      </div>
+
+      <div className="form-container sign-in">
+        <form>
+          <h1>Entrar</h1>
+          <div className="social-icons">
+            <a href="#" className="icon">
+              <i className="fa-brands fa-google"></i>
+            </a>
+          </div>
+          <span>ou use seu e-mail e senha</span>
+          <input type="email" placeholder="E-mail" />
+          <input type="password" placeholder="Senha" />
+          <a href="#">Esqueceu sua senha?</a>
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Já tem sua conta?</h1>
+            <p>Use seu e-mail cadastrado para acessar todos os recursos do site</p>
+            <button className="hidden" onClick={handleLoginClick}>
+              Entrar
+            </button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>Novo por aqui?</h1>
+            <p>Cadastre-se com seus dados pessoais para aproveitar todos os recursos do site</p>
+            <button className="hidden" onClick={handleRegisterClick}>
+              Cadastre-se
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
