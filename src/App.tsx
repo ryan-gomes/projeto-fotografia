1891369f99ff831e962051294a6bc86c6e9b6e77
Ryan Gomes <ryang3550@gmail.com>
2025-11-06 17:08:23 -0300

    Desisti desse

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
 import Home from './pages/Home'
+import Login from "./pages/Login";
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <Home/>
+    <Router>
+       <Header /> {/* Header sempre visível */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer /> {/* Footer sempre visível */}
+    </Router>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,9 @@
-import Header from "../components/Header";
 import Body from "../components/Body";
-import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
-    <Header />
     <Body />
-    <Footer />
     </>
   );
 };
